2084a350236977c3ddf73eb8270bcb6e463e0a0e
Mandorenix <findus.stenberg@gmail.com>
2026-08-11 16:35:29 +0200

    Add pathfinding files

---
+++ src/solo_x/pathfinding/flow_field.py
@@ -0,0 +1,3 @@
+class FlowField:
+    def __init__(self, grid):
+        self.grid = grid
---
+++ src/solo_x/pathfinding/grid.py
@@ -0,0 +1,4 @@
+class Grid:
+    def __init__(self, width=100, height=100):
+        self.width = width
+        self.height = height
