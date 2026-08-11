4063a647bcad4ae501cfdefdb484cca225780fce
Mandorenix <findus.stenberg@gmail.com>
2026-08-11 16:59:45 +0200

    Update cleanup.py with cleanup system

--- src/solo_x/systems/cleanup.py
+++ src/solo_x/systems/cleanup.py
@@ -1,5 +1,23 @@
 from solo_x.ecs.system import System
 
+
 class CleanupSystem(System):
-    def update(self, delta_time):
-        pass
+    """Handles cleanup of dead entities and resources"""
+    
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.dead_entities = []
+    
+    def update(self, delta_time: float):
+        """Clean up dead entities"""
+        for entity_id in self.dead_entities:
+            # Remove entity from world
+            # TODO: Implement entity removal
+            pass
+        self.dead_entities = []
+    
+    def mark_for_cleanup(self, entity_id: int):
+        """Mark entity for cleanup"""
+        if entity_id not in self.dead_entities:
+            self.dead_entities.append(entity_id)
